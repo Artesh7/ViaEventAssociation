a0470ec2a1cd9d0009f9a57247c065eee327270e
HCDani <hockdanield@gmail.com>
2025-02-27 13:02:53 +0100

    Operation result class added, also documented

---
+++ src/Core/Tools/ViaEventAssociation.Core.Tool.OperationResult/ViaEventAssociation.Core.Tool.OperationResult/Result.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViaEventAssociation.Core.Tool.OperationResult
+{
+    public class Result<T>
+    {
+        int resultCode; // Success is 0, anything else is error.
+        string errorMessage;
+        T payLoad;
+
+        public Result(int resultCode, string errorMessage){
+            this.resultCode = resultCode;
+            this.errorMessage = errorMessage;
+        }
+        public Result(T payload){
+            this.resultCode = 0;
+            this.payLoad = payLoad;
+        }
+
+        public AddError(int resultCode, string errorMessage){
+            this.errorMessage += "," + resultCode + "-" + errorMessage;
+        }
+    }
+}
